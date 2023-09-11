a8f63baa47a118581636d023e5b171a6fc156280
Daniel <merchantdaniel8@gmail.com>
2023-09-11 13:18:47 +0000

    homepage

---
+++ src/Components/firebase.js
@@ -0,0 +1,35 @@
+// // Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+// const firebaseConfig = {
+
+//      apiKey: "AIzaSyC6UDM8O3wlMa5SNLHfcM8MGEFJ3ejc55U",
+
+//     authDomain: "artisan-5c916.firebaseapp.com",
+  
+//     databaseURL: "https://artisan-5c916-default-rtdb.firebaseio.com",
+  
+//     projectId: "artisan-5c916",
+  
+//     storageBucket: "artisan-5c916.appspot.com",
+  
+//     messagingSenderId: "218126545440",
+  
+//     appId: "1:218126545440:web:f2d497522b83c195adc245",
+  
+//     measurementId: "G-YTCSC27785"
+  
+//   // Your Firebase config object with API keys, project IDs, etc.
+// };
+
+// // Initialize Firebase
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Firebase Authentication and get a reference to the service
+// export const auth = getAuth(app);
+// export default app;
